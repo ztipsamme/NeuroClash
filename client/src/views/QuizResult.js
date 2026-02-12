e213199f8bb46ae8b2fdc65367903fef1f5bb97a
ztipsamme <e.spitz@hotmail.com>
2026-02-12 13:13:56 +0100

    style quiz result

--- client/src/views/QuizResult.js
+++ client/src/views/QuizResult.js
@@ -1,25 +1,30 @@
+import { addStylesheet } from '../core/utils.js'
 import { getQuizById, getQuizResult } from '../services/quizService.js'
 
 export default function QuizResult({ id }) {
+  addStylesheet('quiz-result-css', '/components/quiz-result.css')
+
   queueMicrotask(() => init(id))
 
   return /* html */ `
     <div id="quiz-result">
       <h1 id="quiz-title">Loading quiz...</h1>
-
-      <table>
-        <tr>
-          <th>Time</th>
-          <th>Correct</th>
-          <th>Incorrect</th>
-        </tr>
-        <tr>
-          <th id="quiz-time"></th>
-          <th id="quiz-correct"></th>
-          <th id="quiz-incorrect"></th>
-        </tr>
-      </table>
-      <a href="/">Back to Home</a>
+      <div class="card result-card">
+        <h2>Result</h2>
+        <table class="result-table">
+          <tr>
+            <th>Time</th>
+            <th>Correct</th>
+            <th>Incorrect</th>
+          </tr>
+          <tr>
+            <th id="quiz-time"></th>
+            <th id="quiz-correct"></th>
+            <th id="quiz-incorrect"></th>
+          </tr>
+        </table>
+        <a href="/" class="button">Back to Home</a>
+      </div>
     </div>
   `
 }
@@ -29,9 +34,10 @@ const init = async (id) => {
     const quiz = await getQuizById(id)
     const result = await getQuizResult(id)
 
-    document.querySelector('#quiz-title').textContent = `Result: ${quiz.title}`
+    document.querySelector('#quiz-title').textContent = quiz.title
 
-    document.querySelector('#quiz-time').textContent = result.totalTimePlayed
+    document.querySelector('#quiz-time').textContent =
+      `${result.totalTimePlayed} s`
     document.querySelector('#quiz-correct').textContent = result.correctAnswers
     document.querySelector('#quiz-incorrect').textContent =
       result.incorrectAnswers
